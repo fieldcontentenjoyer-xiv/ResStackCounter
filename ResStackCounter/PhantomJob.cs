7967c024c42b822a4f2e531f98b5eda0eafc0295
fieldcontentenjoyer-xiv <49669870+fieldcontentenjoyer-xiv@users.noreply.github.com>
2025-09-19 00:04:32 +0200

    Slight code cleanup

--- ResStackCounter/PhantomJob.cs
+++ ResStackCounter/PhantomJob.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
-namespace ResStackCounter.Windows;
+namespace ResStackCounter;
 
 public class PhantomJob
 {
@@ -13,20 +14,22 @@ public class PhantomJob
         PhantomJobName = phantomJobName;
     }
 
-    public static readonly List<PhantomJob> PhantomJobs =
-    [
-        new(4358, "Knight"),
-        new(4360, "Monk"),
-        new(4369, "Thief"),
-        new(4362, "Samurai"),
-        new(4359, "Berserker"),
-        new(4361, "Ranger"),
-        new(4365, "Time Mage"),
-        new(4367, "Chemist"),
-        new(4364, "Geomancer"),
-        new(4363, "Bard"),
-        new(4368, "Oracle"),
-        new(4366, "Cannoneer"),
-        new(4242, "Freelancer"),
-    ];
+    public static readonly Dictionary<int, PhantomJob> PhantomJobLookupTable = new()
+    {
+        { 4358, new PhantomJob(4358, "Knight") },
+        { 4360, new PhantomJob(4360, "Monk") },
+        { 4369, new PhantomJob(4369, "Thief") },
+        { 4362, new PhantomJob(4362, "Samurai") },
+        { 4359, new PhantomJob(4359, "Berserker") },
+        { 4361, new PhantomJob(4361, "Ranger") },
+        { 4365, new PhantomJob(4365, "Time Mage") },
+        { 4367, new PhantomJob(4367, "Chemist") },
+        { 4364, new PhantomJob(4364, "Geomancer") },
+        { 4363, new PhantomJob(4363, "Bard") },
+        { 4368, new PhantomJob(4368, "Oracle") },
+        { 4366, new PhantomJob(4366, "Cannoneer") },
+        { 4242, new PhantomJob(4242, "Freelancer") }
+    };
+
+    public static readonly List<PhantomJob> PhantomJobs = PhantomJobLookupTable.Values.ToList();
 }
--- ResStackCounter/Windows/MainWindow.cs
+++ ResStackCounter/Windows/MainWindow.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.Runtime.InteropServices;
 using Dalamud.Bindings.ImGui;
 using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Interface.Textures;
@@ -12,7 +11,6 @@ using Dalamud.Utility;
 using ECommons;
 using ECommons.ImGuiMethods;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
-using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using Status = Dalamud.Game.ClientState.Statuses.Status;
 
 
@@ -103,220 +101,222 @@ public class MainWindow : Window, IDisposable
 
     private void DrawStatusTable()
     {
-        try
+        if (ImGui.BeginTable("StatusTable", 5, ImGuiTableFlags.RowBg | ImGuiTableFlags.SizingStretchSame))
         {
-            if (ImGui.BeginTable("StatusTable", 5, ImGuiTableFlags.RowBg | ImGuiTableFlags.SizingStretchSame))
+            for (var i = 0; i < (Configuration.ShowMastery ? 5 : 4); i++)
             {
-                for (var i = 0; i < (Configuration.ShowMastery ? 5 : 4); i++)
+                ImGui.TableSetupColumn($"Col{i}");
+            }
+
+            DrawTableHeader();
+
+            var playerDataList = RetrievePlayerList();
+            SortList(playerDataList, Configuration.SortingOrder);
+            playerDataList.ForEach(DrawPlayerRow);
+
+            ImGui.EndTable();
+        }
+    }
+
+    private void DrawTableHeader()
+    {
+        ImGui.TableNextColumn();
+        ImGui.Text("Class/Name");
+        ImGui.SetNextItemWidth(150f);
+        if (ImGui.BeginCombo("##jobtype", JobTypeSelectedFilterOptions))
+        {
+            var tankRef = Configuration.JobTypeFilter[0];
+            if (ImGui.Checkbox("Tanks", ref tankRef))
+            {
+                Configuration.JobTypeFilter[0] = tankRef;
+                Configuration.Save();
+            }
+
+            var healRef = Configuration.JobTypeFilter[1];
+            if (ImGui.Checkbox("Healers", ref healRef))
+            {
+                Configuration.JobTypeFilter[1] = healRef;
+                Configuration.Save();
+            }
+
+            var dpsRef = Configuration.JobTypeFilter[2];
+            if (ImGui.Checkbox("DPS", ref dpsRef))
+            {
+                Configuration.JobTypeFilter[2] = dpsRef;
+                Configuration.Save();
+            }
+
+            ImGui.EndCombo();
+        }
+
+        ImGui.SetNextItemWidth(120f);
+        ImGui.InputTextWithHint("##namefilter", "Name Filter...", ref nameFilter, 21);
+        ImGui.TableNextColumn();
+        ImGui.Text("Phantom Job");
+        ImGui.SetNextItemWidth(100f);
+        if (ImGui.BeginCombo("##pj", "Select..."))
+        {
+            var allPhantomJobs = Configuration.AllPhantomJobs;
+            if (ImGuiEx.Checkbox("Toggle all", ref allPhantomJobs))
+            {
+                Configuration.AllPhantomJobs = allPhantomJobs;
+                Configuration.Save();
+            }
+
+            ImGui.Separator();
+
+            foreach (var phantomJob in Configuration.PhantomJobFilter)
+            {
+                if (ImGui.Checkbox(phantomJob.PhantomJob.PhantomJobName, ref phantomJob.Visible))
                 {
-                    ImGui.TableSetupColumn($"Col{i}");
+                    plugin.Config.Save();
                 }
+            }
 
-                ImGui.TableNextColumn();
-                ImGui.Text("Class/Name");
-                ImGui.SetNextItemWidth(150f);
-                if (ImGui.BeginCombo("##jobtype", JobTypeSelectedFilterOptions))
+            ImGui.EndCombo();
+        }
+
+        if (Configuration.ShowMastery)
+        {
+            ImGui.TableNextColumn();
+            ImGui.Text("Mastery");
+        }
+
+        ImGui.TableNextColumn();
+        ImGui.Text("Res Stacks");
+        ImGui.SetNextItemWidth(80f);
+        if (ImGui.BeginCombo(
+                "##count", ResStackSelectedFilterOptions))
+        {
+            for (var i = 3; i >= 0; i--)
+            {
+                if (ImGui.Checkbox(i.ToString(), ref Configuration.ResStackCountFilter[i]))
                 {
-                    var tankRef = Configuration.JobTypeFilter[0];
-                    if (ImGui.Checkbox("Tanks", ref tankRef))
-                    {
-                        Configuration.JobTypeFilter[0] = tankRef;
-                        Configuration.Save();
-                    }
-
-                    var healRef = Configuration.JobTypeFilter[1];
-                    if (ImGui.Checkbox("Healers", ref healRef))
-                    {
-                        Configuration.JobTypeFilter[1] = healRef;
-                        Configuration.Save();
-                    }
-
-                    var dpsRef = Configuration.JobTypeFilter[2];
-                    if (ImGui.Checkbox("DPS", ref dpsRef))
-                    {
-                        Configuration.JobTypeFilter[2] = dpsRef;
-                        Configuration.Save();
-                    }
-
-                    ImGui.EndCombo();
+                    plugin.Config.Save();
                 }
+            }
 
-                ImGui.SetNextItemWidth(120f);
-                ImGui.InputTextWithHint("##namefilter", "Name Filter...", ref nameFilter, 21);
-                ImGui.TableNextColumn();
-                ImGui.Text("Phantom Job");
-                ImGui.SetNextItemWidth(100f);
-                if (ImGui.BeginCombo("##pj", "Select..."))
+            ImGui.EndCombo();
+        }
+
+        ImGui.TableNextColumn();
+        ImGui.Text("Knowledge Level");
+        var hideLevel20Players = Configuration.HideLevel20PlayersFilter;
+        if (ImGui.Checkbox("Hide Level 20", ref hideLevel20Players))
+        {
+            Configuration.HideLevel20PlayersFilter = hideLevel20Players;
+            Configuration.Save();
+        }
+
+        ImGui.TableNextRow();
+    }
+
+    private List<PlayerDataStructure> RetrievePlayerList()
+    {
+        List<PlayerDataStructure> playerDataList = [];
+        foreach (var player in Plugin.ObjectTable.PlayerObjects)
+        {
+            Status? mastery = null;
+            Status? phantomJob = null;
+            Status? resStacks = null;
+
+            foreach (var status in player.StatusList)
+            {
+                if (Configuration.ShowMastery && IsMasteryBuff(status.StatusId))
                 {
-                    var allPhantomJobs = Configuration.AllPhantomJobs;
-                    if (ImGuiEx.Checkbox("Toggle all", ref allPhantomJobs))
-                    {
-                        Configuration.AllPhantomJobs = allPhantomJobs;
-                        Configuration.Save();
-                    }
-
-                    ImGui.Separator();
-
-                    foreach (var phantomJob in Configuration.PhantomJobFilter)
-                    {
-                        if (ImGui.Checkbox(phantomJob.PhantomJob.PhantomJobName, ref phantomJob.Visible))
-                        {
-                            plugin.Config.Save();
-                        }
-                    }
-
-                    ImGui.EndCombo();
+                    mastery = status;
                 }
-
-                if (Configuration.ShowMastery)
+                else if (IsPhantomJobBuff(status.StatusId))
                 {
-                    ImGui.TableNextColumn();
-                    ImGui.Text("Mastery");
+                    phantomJob = status;
                 }
-
-                ImGui.TableNextColumn();
-                ImGui.Text("Res Stacks");
-                ImGui.SetNextItemWidth(80f);
-                if (ImGui.BeginCombo(
-                        "##count", ResStackSelectedFilterOptions))
+                else if (IsResRestrictedDebuff(status))
                 {
-                    for (var i = 3; i >= 0; i--)
-                    {
-                        if (ImGui.Checkbox(i.ToString(), ref Configuration.ResStackCountFilter[i]))
-                        {
-                            plugin.Config.Save();
-                        }
-                    }
-
-                    ImGui.EndCombo();
+                    resStacks = status;
                 }
+            }
 
-                ImGui.TableNextColumn();
-                ImGui.Text("Knowledge Level");
-                var hideLevel20Players = Configuration.HideLevel20PlayersFilter;
-                if (ImGui.Checkbox("Hide Level 20", ref hideLevel20Players))
-                {
-                    Configuration.HideLevel20PlayersFilter = hideLevel20Players;
-                    Configuration.Save();
-                }
-
-                ImGui.TableNextRow();
-
-                List<PlayerDataStructure> playerDataList = [];
-                foreach (var player in Plugin.ObjectTable.PlayerObjects)
-                {
-                    Status? mastery = null;
-                    Status? phantomJob = null;
-                    Status? resStacks = null;
-
-                    foreach (var status in player.StatusList)
-                    {
-                        if (Configuration.ShowMastery && IsMasteryBuff(status.StatusId))
-                        {
-                            mastery = status;
-                        }
-                        else if (IsPhantomJobBuff(status.StatusId))
-                        {
-                            phantomJob = status;
-                        }
-                        else if (IsResRestrictedDebuff(status))
-                        {
-                            resStacks = status;
-                        }
-                    }
-
-                    var knowledgeLevel = GetKnowledgeLevel(player);
-                    if (ShouldShow(phantomJob, resStacks, player, knowledgeLevel))
-                    {
-                        playerDataList.Add(
-                            new PlayerDataStructure(player, mastery, phantomJob!, resStacks, knowledgeLevel));
-                    }
-                }
-
-                SortList(playerDataList, Configuration.SortingOrder);
-
-                foreach (var playerData in playerDataList)
-                {
-                    var player = playerData.Item1;
-                    var mastery = playerData.Item2;
-                    var phantomJob = playerData.Item3;
-                    var resStacks = playerData.Item4;
-                    var knowledgeLevel = playerData.Item5;
-
-                    ImGui.TableNextColumn();
-                    ImGui.Image(
-                        Plugin.TextureProvider.GetFromGameIcon(62100u + player.ClassJob.RowId).GetWrapOrEmpty()
-                              .Handle,
-                        new Vector2(32, 32));
-                    ImGui.SameLine();
-                    ImGui.Text(player.Name.TextValue);
-                    ImGui.SameLine();
-
-                    ImGui.TableNextColumn();
-                    var pjIcon = Plugin.TextureProvider
-                                       .GetFromGameIcon(new GameIconLookup(phantomJob.GameData.Value.Icon))
-                                       .GetWrapOrEmpty();
-                    ImGui.Image(pjIcon.Handle, IconSize);
-
-                    if (Configuration.ShowMastery)
-                    {
-                        ImGui.TableNextColumn();
-                        var masteryIconId =
-                            mastery != null ? mastery.GameData.Value.Icon + mastery.Param - 1 : MissingMasteryIconId;
-
-                        var masteryIcon = Plugin.TextureProvider
-                                                .GetFromGameIcon(new GameIconLookup(masteryIconId))
-                                                .GetWrapOrEmpty();
-                        ImGui.Image(masteryIcon.Handle, IconSize);
-                    }
-
-                    ImGui.TableNextColumn();
-
-                    if (resStacks != null)
-                    {
-                        var resStacksIconId = resStacks.StatusId == ResurrectionDeniedBuffId
-                                                  ? resStacks.GameData.Value.Icon
-                                                  : resStacks.GameData.Value.Icon + resStacks.Param - 1;
-                        var resStacksIcon = Plugin.TextureProvider
-                                                  .GetFromGameIcon(new GameIconLookup(resStacksIconId))
-                                                  .GetWrapOrEmpty();
-                        ImGui.Image(resStacksIcon.Handle, IconSize);
-                    }
-                    else
-                    {
-                        ImGui.Text("-");
-                    }
-
-                    ImGui.TableNextColumn();
-                    ImGui.Text(knowledgeLevel.ToString());
-
-                    ImGui.TableNextRow();
-                }
-
-                ImGui.EndTable();
+            var knowledgeLevel = GetKnowledgeLevel(player);
+            if (ShouldShow(phantomJob, resStacks, player, knowledgeLevel))
+            {
+                playerDataList.Add(new PlayerDataStructure(player, mastery, phantomJob, resStacks, knowledgeLevel));
             }
         }
-        catch (Exception e)
+
+        return playerDataList;
+    }
+
+    private void DrawPlayerRow(PlayerDataStructure playerData)
+    {
+        var player = playerData.Item1;
+        var mastery = playerData.Item2;
+        var phantomJob = playerData.Item3;
+        var resStacks = playerData.Item4;
+        var knowledgeLevel = playerData.Item5;
+
+        ImGui.TableNextColumn();
+        ImGui.Image(
+            Plugin.TextureProvider.GetFromGameIcon(ClassJobIconBaseId + player.ClassJob.RowId).GetWrapOrEmpty()
+                  .Handle,
+            new Vector2(32, 32));
+        ImGui.SameLine();
+        ImGui.Text(player.Name.TextValue);
+        ImGui.SameLine();
+
+        ImGui.TableNextColumn();
+        var pjIcon = Plugin.TextureProvider
+                           .GetFromGameIcon(new GameIconLookup(phantomJob.GameData.Value.Icon))
+                           .GetWrapOrEmpty();
+        ImGui.Image(pjIcon.Handle, IconSize);
+
+        if (Configuration.ShowMastery)
         {
-            Plugin.Log.Error(e.ToString());
+            ImGui.TableNextColumn();
+            var masteryIconId =
+                mastery != null ? mastery.GameData.Value.Icon + mastery.Param - 1 : MissingMasteryIconId;
+
+            var masteryIcon = Plugin.TextureProvider
+                                    .GetFromGameIcon(new GameIconLookup(masteryIconId))
+                                    .GetWrapOrEmpty();
+            ImGui.Image(masteryIcon.Handle, IconSize);
         }
+
+        ImGui.TableNextColumn();
+
+        if (resStacks != null)
+        {
+            var resStacksIconId = resStacks.StatusId == ResurrectionDeniedBuffId
+                                      ? resStacks.GameData.Value.Icon
+                                      : resStacks.GameData.Value.Icon + resStacks.Param - 1;
+            var resStacksIcon = Plugin.TextureProvider
+                                      .GetFromGameIcon(new GameIconLookup(resStacksIconId))
+                                      .GetWrapOrEmpty();
+            ImGui.Image(resStacksIcon.Handle, IconSize);
+        }
+        else
+        {
+            ImGui.Text("-");
+        }
+
+        ImGui.TableNextColumn();
+        ImGui.Text(knowledgeLevel.ToString());
+
+        ImGui.TableNextRow();
     }
 
     private static bool IsMasteryBuff(uint buffId)
     {
-        return buffId == 4226;
+        return buffId == MasteryBuffId;
     }
 
     private static bool IsPhantomJobBuff(uint buffId)
     {
-        return buffId is >= 4358 and <= 4369 or 4242;
+        return PhantomJob.PhantomJobs.Any(pj => pj.BuffId == buffId);
     }
 
     private static bool IsResRestrictedDebuff(Status status)
     {
-        // var iconId = status.GameData.Value.Icon;
-        // return iconId is ResurrectionDeniedIconId or >= 218286 and <= 218288;
-        return status.StatusId is 4262 or 4263;
+        return status.StatusId is ResurrectionRestrictedBuffId or ResurrectionDeniedBuffId;
     }
 
     private bool ShouldShow(Status? phantomJob, Status? resStacks, IBattleChara player, byte knowledgeLevel)
@@ -349,8 +349,11 @@ public class MainWindow : Window, IDisposable
 
         var playerJobFilterIndex = player.ClassJob.Value.Role switch
         {
+            // Tanks
             1 => 0,
+            // Healers
             4 => 1,
+            // Other, Including Melee/Ranged
             _ => 2
         };
 
@@ -410,6 +413,13 @@ public class MainWindow : Window, IDisposable
 
     private const int ResurrectionDeniedBuffId = 4263;
 
+    private const int ResurrectionRestrictedBuffId = 4262;
+
+    private const int MasteryBuffId = 4226;
+
+    private const uint ClassJobIconBaseId = 62100u;
+
+    // Tanks -> Healers -> Melee -> Ranged -> Caster
     private static readonly byte[] ClassOrder =
         [1, 3, 12, 17, 6, 13, 9, 20, 4, 10, 14, 19, 21, 5, 11, 18, 7, 8, 15, 22];
 }
